aa024ceb3315f8a9a082e83af956a3b644868a81
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2015-03-17 12:54:06 +0100

    GH-PAGES

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-# bundle js
-node_modules/browserify/bin/cmd.js --debug -t [ babelify --playground ] -o dist/js/webplayer.js src/js/webplayer.js;
-# bundle css
-# cp for now
-cp src/css/style.css dist/css/style.min.css
--- src/js/webplayer.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Player from './components/player.jsx';
-
-let div = document.getElementById( 'webplayer' );
-React.render( <Player/>, div );
